0d8a30ad343e5c838c7935b9513e1ef8bff2ca51
Diego Goulart <diego@djgoulart.com.br>
2016-11-03 04:59:34 -0200

    add login and logout for admins

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -47,4 +47,22 @@ class LoginController extends Controller
     }
 
 
+    /**
+     * Log the user out of the application.
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->flush();
+
+        $request->session()->regenerate();
+
+        return redirect(env('URL_ADMIN_LOGIN'));
+    }
+
+
 }
